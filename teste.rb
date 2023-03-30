0a4c10a2560fe57968209277b37824378dac8464
Benja492 <95471855+Benja492@users.noreply.github.com>
2023-03-30 09:28:30 -0300

    Add files via upload

---
+++ teste.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome: "
+nome= gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+print "Olá #{nome} #{sobrenome}, de #{idade} anos." 
